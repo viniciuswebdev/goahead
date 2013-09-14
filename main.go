b1d688ef90d5e4d690b18795ad5abae61f04c238
vinicius <viniciuswebdev@gmail.com>
2013-09-14 14:55:14 -0300

    Should get hash from url

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
     db := database.Database{"root", "abc123", "zocprint"}
-    http.Redirect(w, r, db.FindShortenerUrlByHash("zocprint"), 301)
+    http.Redirect(w, r, db.FindShortenerUrlByHash(r.URL.Path[1:]), 301)
 }
 
 func main() {
