4be01a6ce7cdaed6f88c9ca84802149ff8575915
cloudson <claudsonweb@gmail.com>
2013-09-27 00:50:57 -0300

    Add support to fastcgi (#6)

--- main.go
+++ main.go
@@ -4,34 +4,16 @@ import (
 	"code.google.com/p/gcfg"
 	"flag"
 	"github.com/viniciuswebdev/goahead/database"
-	"log"
-	"net/http"
+	"github.com/viniciuswebdev/goahead/server"
 )
 
 type Config struct {
-	General struct {
-		Port string
-	}
+	General server.Server
 	Database database.DatabaseConf
 	Table database.TableConf
 }
 
 var _cfg Config
-var _db *database.Database
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	hash := r.URL.Path[1:]
-	log.Printf("Searching url with hash '%s' \n", hash)
-
-	url, error := _db.FindShortenerUrlByHash(hash, &(_cfg.Table))
-	if error != nil {
-		log.Printf("%s \n", error.Error())
-		http.NotFound(w, r)
-		return
-	}
-	http.Redirect(w, r, url, http.StatusMovedPermanently)
-}
-
 func main() {
 	configFilePath := flag.String("config", "./etc/goahead.ini", "Configuration file path")
 	flag.Parse()
@@ -40,16 +22,10 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	_db = database.Create(&(_cfg.Database))
-	err = _db.IsValid() 
+	db := database.Create(&(_cfg.Database))
+	err = db.IsValid() 
 	if err != nil {
 		panic(err.Error())
 	}
-
-	http.HandleFunc("/", handler)
-	log.Printf("Starting Goahead on localhost:%s ...\n", _cfg.General.Port)
-	err = http.ListenAndServe(":"+_cfg.General.Port, nil)
-	if err != nil {
-		panic(err.Error())
-	}
+	_cfg.General.TurnOn(db, &(_cfg.Table))
 }
---
+++ server/server.go
@@ -0,0 +1,75 @@
+package server 
+
+import (
+    "net"
+    "net/http"
+    "net/http/fcgi"
+    "log"
+    "github.com/viniciuswebdev/goahead/database"
+)
+
+type Server struct {
+    Port string
+    Host string 
+    FastCgi bool 
+}
+
+var _db *database.Database
+var _table *database.TableConf
+
+func (server *Server) initialize(db *database.Database, table *database.TableConf) {
+    _db = db 
+    _table = table
+    if server.Port == "" {
+        server.Port = "9000"
+    }
+
+    if server.Host == "" {
+        server.Host = "localhost"
+    }
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    hash := r.URL.Path[1:]
+    log.Printf("Searching url with hash '%s' \n", hash)
+
+    url, error := _db.FindShortenerUrlByHash(hash, _table)
+    if error != nil {
+        log.Printf("%s \n", error.Error())
+        http.NotFound(w, r)
+        return
+    }
+    http.Redirect(w, r, url, http.StatusMovedPermanently)
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    handler(w, r)   
+}
+
+func (s *Server) TurnOn(db *database.Database, table *database.TableConf) {
+    s.initialize(db, table)
+    if s.FastCgi {
+        log.Printf("Starting Goahead on %s:%s using fastcgi...\n", s.Host, s.Port)
+        s.turnOnFastCGI()
+        return 
+    }
+    log.Printf("Starting Goahead on %s:%s ...\n", s.Host, s.Port)
+    s.turnOnSimple()
+}
+
+func (s *Server) turnOnSimple() {
+    http.HandleFunc("/", handler)
+    var err = http.ListenAndServe(":"+s.Port, nil)
+    if err != nil {
+        panic(err.Error())
+    }
+}
+
+func (s *Server) turnOnFastCGI() {
+    listener, err := net.Listen("tcp", s.Host+":"+s.Port)
+    if err != nil {
+        panic(err.Error())
+    }
+    srv := new(Server)
+    fcgi.Serve(listener, srv)
+}
