fd125b8946c9d0e3fba8fab3cc527f9706af0706
cloudson <claudsonweb@gmail.com>
2013-10-03 00:25:06 -0300

    Add flags run, help and build

--- main.go
+++ main.go
@@ -3,31 +3,73 @@ package main
 import (
 	"code.google.com/p/gcfg"
 	"flag"
-	"log"
+	"fmt"
 	"github.com/viniciuswebdev/goahead/database"
 	"github.com/viniciuswebdev/goahead/server"
+	"log"
 )
 
 type Config struct {
-	General server.Server
+	General  server.Server
 	Database database.DatabaseConf
-	Table database.TableConf
-	Cache database.CacheConf
+	Table    database.TableConf
+	Cache    database.CacheConf
 }
 
 var _cfg Config
+
 func main() {
 	configFilePath := flag.String("config", "./etc/goahead.ini", "Configuration file path")
+	help := flag.Bool("help", false, "Show me the help!")
+	run := flag.Bool("run", false, "Run server")
+	build := flag.Bool("build", false, "Create the scaffold")
+
 	flag.Parse()
+
+	if *help || (!(*run) && !(*build)) {
+		showHelp()
+		return
+	}
 
 	err := gcfg.ReadFileInto(&_cfg, *configFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	db := database.Create(&(_cfg.Database))
-	err = db.IsValid() 
+	err = db.IsValid()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if *build {
+		buildScaffold(&_cfg)
+		return
+	}
+
+	if *run {
+		runServer(db)
+		return
+	}
+
+}
+
+func showHelp() {
+	fmt.Println("Goahead is a simple service that redirects routes\n")
+	fmt.Println("Usage: ")
+	fmt.Println("\t goahead [argument]")
+	fmt.Println("")
+	fmt.Println("The commands are: ")
+	fmt.Println("\t -help      show the help")
+	fmt.Println("\t -build     create the scaffold")
+	fmt.Println("\t -run       [-config filepath with ini configuration]")
+	fmt.Println("")
+	fmt.Println("Visit http://github.com/viniciuswebdev/goahead for many informations")
+}
+
+func buildScaffold(config *Config) {
+	fmt.Println("Creating the scaffold...")
+}
+
+func runServer(db *database.Database) {
 	_cfg.General.TurnOn(db, &(_cfg.Table), &(_cfg.Cache))
 }
